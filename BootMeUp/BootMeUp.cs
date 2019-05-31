49662b192220385ac04e8ef6cd2a607feb45991c
Willy-Kimura <wilskym@live.com>
2019-05-31 10:56:31 +0300

    Add project files.

---
+++ BootMeUp/BootMeUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WK.Libraries.BootMeUpNS
+{
+    public partial class BootMeUp : Component
+    {
+        public BootMeUp()
+        {
+            InitializeComponent();
+        }
+
+        public BootMeUp(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
